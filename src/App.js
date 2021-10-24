8bab8e6d6ab625dea273ba027cbddcb4686e2a95
easymagic <nnamware@yahoo.com>
2021-10-24 22:39:14 +0100

    np

--- src/App.js
+++ src/App.js
@@ -1,36 +1,45 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+// import { useState } from 'react';
 
 function App() {
 
-  const [a,b] = useState(10);
+  // const [a,b] = useState(10);
   
-  const bindInc = ()=>{
-    return {
-      onClick:()=>b(a + 1)
-    };
-  }
+  // const bindInc = ()=>{
+  //   return {
+  //     onClick:()=>b(a + 1)
+  //   };
+  // }
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <button {...bindInc()}>Inc {a}</button>
-      </header>
-    </div>
-  );
+      <>
+      <div className="container mx-auto p-2">
+
+           <div className="bg-blue-500 p-2 flex justify-between">
+             <div className="text-white font-bold">
+               Twittie
+             </div>
+             <div className="text-white text-xs mt-1 flex">
+               <a href="p" className="mx-2">
+                 Login
+               </a>
+               <a href="p"  className="mx-2">
+                 Signup
+               </a>
+               <a href="p"  className="mx-2">
+                 Profile
+               </a>
+               <a href="p"  className="mx-2">
+                 Logout
+               </a>
+             </div>
+           </div>
+
+
+      </div>
+      </>
+    );
 }
 
 export default App;
